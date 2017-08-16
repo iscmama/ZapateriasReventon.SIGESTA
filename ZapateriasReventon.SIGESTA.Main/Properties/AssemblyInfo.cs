8b386906ba0a50b63b3cc81c7249347e16041572
iscmama <iscmama@hotmail.com>
2017-08-15 20:50:48 -0500

    add publish app

--- ZapateriasReventon.SIGESTA.Main/Properties/AssemblyInfo.cs
+++ ZapateriasReventon.SIGESTA.Main/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZapateriasReventon.SIGESTA.Main")]
+[assembly: AssemblyTitle("Zapaterias Reventon SIGESTA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ZapateriasReventon.SIGESTA.Main")]
+[assembly: AssemblyCompany("Umbrella Developers")]
+[assembly: AssemblyProduct("SIGESTA")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
